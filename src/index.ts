39a7fd850b17d32b808ed9f9bb3c21b62e5e16ee
MrlingXD <wlingzhenyu@163.com>
2025-07-31 14:01:12 +0800

    fix: update command shortcut to alias for consistency

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ export const schema: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   ctx.command('thursday', '随机输出 KFC 疯狂星期四段子')
-    .shortcut('疯狂星期四')
+    .alias('疯狂星期四')
     .action(() => Random.pick(corpus))
 }

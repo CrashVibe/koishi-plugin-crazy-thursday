417435c27d37203f143089ed8b4c6dc3a3aa8922
MrlingXD <90316914+wling-art@users.noreply.github.com>
2025-10-14 19:26:56 +0800

    🎨 run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { Context, Random, Schema } from 'koishi'
+import { Context, Random, Schema } from "koishi";
 
-const corpus: string[] = require('../corpus')
+const corpus: string[] = require("../corpus");
 
 export interface Config {}
 
-export const name = 'crazy-thursday'
-export const schema: Schema<Config> = Schema.object({})
+export const name = "crazy-thursday";
+export const schema: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  ctx.command('thursday', '随机输出 KFC 疯狂星期四段子')
-    .alias('疯狂星期四')
-    .action(() => Random.pick(corpus))
+    ctx.command("thursday", "随机输出 KFC 疯狂星期四段子")
+        .alias("疯狂星期四")
+        .action(() => Random.pick(corpus));
 }
